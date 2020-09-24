6558be0b8d9f827486c204b89780ae691000e5ef
primo9293 <42128291+primo9293@users.noreply.github.com>
2020-09-23 19:51:06 -0500

    Inicio App

---
+++ src/hello.ts
@@ -0,0 +1,5 @@
+console.log('Hi');
+// tsc --init  Inicializa
+// tsc  Corre el archivod e configuración de Typescript tsconfig,json
+// tsc --watch Esta atento a los cambios guardados y los compila
+// node dist/hello.js  ejecuta en node
